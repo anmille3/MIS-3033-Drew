ddee5f1e80b7924fbe7d582c056f7889fd2a3bef
Drew Miller <Andrew.N.Miller-1@ou.edu>
2020-08-31 10:06:12 -0700

    Yup.

---
+++ Review/ClassesFilesDateTime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesFilesDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle();
+            rect1.width = 10;
+            rect1.height = 20;
+
+            Rectangle rect2 = new Rectangle(5, 2);
+
+            Console.WriteLine($"The area of the rect1 is {rect1.area()}");
+            Console.WriteLine($"The area of the rect2 is {rect2.area()}");
+
+            List<Rectangle> rectangles = new List<Rectangle>();
+            rectangles.Add(rect1);
+            rectangles.Add(rect2);
+
+            //Add a rectanlge
+            rectangles.Add(new Rectangle(7, 7));
+
+            foreach (var rect in rectangles)
+            {
+                Console.WriteLine(rect);
+                //Console.WriteLine($"The area of the rectangle is {rect.area()}");
+            }
+
+        }
+    }
+}
---
+++ Review/ClassesFilesDateTime/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesFilesDateTime
+{
+
+    public class Rectangle
+    {
+        public int width { get; set; }
+
+        public int height { get; set; }
+        
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            creationDate = DateTime.Now;
+
+        }
+
+        public DateTime creationDate { get; set; }
+
+        public Rectangle()
+        {
+        }
+
+        public double area()
+        {
+            return width * height;
+        }
+
+        public override string ToString()
+        {
+            var diff = DateTime.Now = creationDate;
+            string output = $"The rectangle has a width of {width} and height of {height}" + $"The rectangle was created {diff.TotalSeconds}";
+        }
+    }
+    
+}
